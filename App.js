660c78b0406241c90607fa547cc80ba0923ded75
Furqan Khan <20035865@stu.mmu.ac.uk>
2023-04-30 21:53:55 +0100

    components updated

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, TextInput, View, Button, Alert, StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack’;
+import { NavigationContainer } from '@react-navigation/native';
+import { UserContextProvider } from './context/UserContext';
+import Tabs from './app/navigation/tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    let date = new Date();
+    let seconds = date.getSeconds();
+    setTimeout(() => {
+        setInterval(() => {
+            drafts.pollForTimedDrafts();
+        }, 60 * 1000);
+    }, (60 - seconds) * 1000);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+        <UserContextProvider>
+            <NavigationContainer>
+                <Tabs />
+            </NavigationContainer>
+        </UserContextProvider>
+    );
+};
+
+export default App;
+
