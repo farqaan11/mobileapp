d2cafafa2cff291813845850870172a054f809cd
Furqan Khan <20035865@stu.mmu.ac.uk>
2023-05-08 22:35:04 +0100

    app file fix

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import { Text, TextInput, View, Button, Alert, StyleSheet } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack’;
-import { NavigationContainer } from '@react-navigation/native';
-import { UserContextProvider } from './context/UserContext';
-import Tabs from './app/navigation/tabs';
+
+import { NavigationContainer } from "@react-navigation/native"
+import { UserContextProvider } from "./context/UserContext"
+import drafts from "./helpers/drafts"
+import Tabs from "./app/navi/TabNav"
 
 const App = () => {
-    let date = new Date();
-    let seconds = date.getSeconds();
-    setTimeout(() => {
-        setInterval(() => {
-            drafts.pollForTimedDrafts();
-        }, 60 * 1000);
-    }, (60 - seconds) * 1000);
+  let date = new Date()
+  let seconds = date.getSeconds()
+  setTimeout(() => {
+    setInterval(() => {
+      drafts.pollForTimedDrafts()
+    }, 60 * 1000)
+  }, (60 - sec) * 1000)
 
-    return (
-        <UserContextProvider>
-            <NavigationContainer>
-                <Tabs />
-            </NavigationContainer>
-        </UserContextProvider>
-    );
-};
+  return (
+    <UserContextProvider>
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
+    </UserContextProvider>
+  )
+}
 
-export default App;
-
+export default App
